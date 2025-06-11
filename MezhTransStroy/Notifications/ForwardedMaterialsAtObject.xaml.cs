49ef8e1c4c82a7ee8db17db76a0469219788cd8d
Кирилл <koolalshir@gmail.com>
2025-06-11 12:01:27 +0300

    minus_text_json

--- MezhTransStroy/Notifications/ForwardedMaterialsAtObject.xaml.cs
+++ MezhTransStroy/Notifications/ForwardedMaterialsAtObject.xaml.cs
@@ -13,7 +13,6 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-using System.Text.Json;
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Wordprocessing;
 using Microsoft.Win32;
